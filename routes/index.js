a2ffabcd3066e4647bd75883841da1006a724dd3
ll3m <ll3m@fresnou.org>
2022-01-21 08:53:16 -0800

    slide 49, everything works other than edit button

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 const productController = require('../controllers/productController.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/products', productController.viewProducts);
 router.get('/products/profile/:id', productController.renderProfile);
+router.get('products/edit/:id', productController.renderEditForm);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
 module.exports = router;
