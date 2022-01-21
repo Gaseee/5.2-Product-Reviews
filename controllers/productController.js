431006cde8caa0c43c0a1ba43480192b3ab90a07
ll3m <ll3m@fresnou.org>
2022-01-21 08:00:44 -0800

    slide 25

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const {Product} = require('../models');
+const categories = ['appliances', 'pet', 'home goods', 'other'];
+
+module.exports.renderProfile = async function(req,res){
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/profile', {product});
+};
+
+module.exports.renderEditForm = async function (req, res){
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit', {product, categories});
+};
