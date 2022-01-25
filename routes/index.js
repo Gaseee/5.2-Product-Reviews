63f3250a70e2de8218815cab02ae72bf1abb7ae6
ll3m <ll3m@fresnou.org>
2022-01-25 08:05:29 -0800

    slide 62, everything works so far

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController.js');
+const reviewController = require('../controllers/reviewController');
 
 /* GET home page. */
 router.get('/products', productController.viewProducts);
 router.get('/products/profile/:id', productController.renderProfile);
-router.get('products/edit/:id', productController.renderEditForm);
+router.get('/products/edit/:id', productController.renderEditForm);
 router.post('/products/edit/:id', productController.updateProduct);
 router.get('/products/add', productController.renderAddForm);
 router.post('/products/add', productController.addProduct);
 router.get('/products/delete/:id', productController.deleteProduct);
+
+
+
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
+router.post('/products/:productId/review/add', reviewController.addReview);
+router.get('/review/edit/:id', reviewController.renderEditReviewForm);
 module.exports = router;
